553c2415c0aa8a34cac69978f754139edfbd9f29
connor-align <cmullen@align.com>
2025-06-09 12:09:07 -0400

    Add files via upload

---
+++ tools/DNS Lookup and Ping/DNS-Reverselookup.ps1
@@ -0,0 +1,27 @@
+﻿$counter = 1
+$comps = get-content '\\wdcnapvs505.internal.cigna.com\Home11$\C8K3JT\Desktop\scripts\Other Tools\DNS Reverse Lookup Script\url-list.txt'
+$dnsResults = "\\wdcnapvs505.internal.cigna.com\Home11$\C8K3JT\Desktop\scripts\Other Tools\DNS Reverse Lookup Scripturl-output.csv"
+
+function get-dnsres{
+foreach ($comp in $comps) {
+$TempIP = ([system.net.dns]::GetHostAddresses($comp)) | select IPAddressToString
+
+$status = "Processing system {0} of {1}: {2}" -f $counter,$comps.Count,$comp
+Write-Progress 'Resolving DNS' $status -PercentComplete ($counter/$comps.count * 100)
+$counter++
+$comp |
+select @{Name='ComputerName';Expression={$comp}}, `
+@{Name='ResolvesToIP';Expression={[system.net.dns]::GetHostAddresses($comp)}}, `
+@{Name='IPResolvesTo';Expression={([system.net.dns]::GetHostEntry($TempIP.IPAddressToString)).HostName}}, `
+@{Name='PingStatus'; Expression={ `
+if ((get-wmiobject -query "SELECT * FROM Win32_PingStatus WHERE Address='$comp'").statuscode -eq 0) {'Host Online'} `
+elseif ((get-wmiobject -query "SELECT * FROM Win32_PingStatus WHERE Address='$comp'").statuscode -eq 11003) {'Destination Host Unreachable'} `
+elseif ((get-wmiobject -query "SELECT * FROM Win32_PingStatus WHERE Address='$comp'").statuscode -eq 11010) {'Request Timed Out'} `
+elseif ((get-wmiobject -query "SELECT * FROM Win32_PingStatus WHERE Address='$comp'").statuscode -eq $Null) {'NoDNS'}
+}
+}
+}
+}
+
+get-dnsres | export-csv $dnsResults -notypeinformation
+invoke-item $dnsResults
